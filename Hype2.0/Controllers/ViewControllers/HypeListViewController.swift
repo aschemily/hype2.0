5a79cc4d91b70743146984d90fa070b425def86e
aschemily <aschemily@gmail.com>
2022-03-01 16:14:45 -0500

    first commit

---
+++ Hype2.0/Controllers/ViewControllers/HypeListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HypeListViewController.swift
+//  Hype2.0
+//
+//  Created by Emily Asch on 3/1/22.
+//
+
+import UIKit
+
+class HypeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hype2.0/Helpers+Extensions/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatter.swift
+//  Hype2.0
+//
+//  Created by Emily Asch on 3/1/22.
+//
+
+import Foundation
